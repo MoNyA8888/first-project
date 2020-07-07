c5361c385d41e2a8fe3bb376d4ab7ea0b32ab693
code for palestine <you@example.com>
2020-07-07 14:48:00 +0300

    add a tage to App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,86 @@
 import React from 'react';
-import logo from './Instagram-Icon.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="logo" alt="logo" />
-        <p>
+import db from "./firebase";
+import './index.css';
+
+class App extends React.Component {
+  TeamMembers() {
+    db.collection("TeamMembers").doc("Monya").set({
+      fullname: "Monya assi",
+      email: "monyaa@gmail.com",
+      favColor: "blue?",
+      address: "palestine",
+    })
+    db.collection("TeamMembers").doc("Hadeel").set({
+      fullname: "Monya assi",
+      email: "monyaa@gmail.com",
+      favColor: "blue?",
+      address: "palestine",
+    })
+    db.collection("TeamMembers").doc("Naem").set({
+      fullname: "Monya assi",
+      email: "monyaa@gmail.com",
+      favColor: "blue?",
+      address: "palestine",
+    })
+    db.collection("TeamMembers").doc("Amro").set({
+      fullname: "Monya assi",
+      email: "monyaa@gmail.com",
+      favColor: "blue?",
+      address: "palestine",
+    })
+
+  }
+  document() {
+    db.collection("TeamMembers")
+      .doc("Monya")
+      .get()
+      .then((doc) => {
+        const data = doc.data()
+        console.log(data)
+      })
+  }
+
+  collection() {
+
+    db.collection("TeamMembers")
+      .get()
+      .then((querySnapshot) => {
+        let data = {}
+        querySnapshot.docs.map(function (doc) {
+          data[ doc.id ] = doc.data()
+          console.log(data)
+          console.log("Monya: " + JSON.stringify(data[ "Monya assi" ]))
+        })
+      }
+
+      )
+
+  }
 
 
-          Right your name
-        </p>
-        <input className="searchbar"></input>
-        <a href='#'  >si up</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          don't click here
-        </a>
-      </header>
-    </div >
-  );
+
+  render() {
+    return (
+
+      <div>
+        <button onClick={this.TeamMembers}></button>
+
+
+
+        <button onClick={this.document}></button>
+
+
+        <button onClick={this.collection}></button>
+      </div>
+
+
+
+
+    );
+  }
 }
+
 
 export default App;
